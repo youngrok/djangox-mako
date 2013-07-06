d6c7f6e5823006fea0f7c22adc6b4ebf2acda253
Youngrok pak <pak.youngrok@gmail.com>
2013-07-06 21:38:46 +0900

    github url

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 setup(name='djangox-mako',
       author='Youngrok Pak',
       author_email='pak.youngrok@gmail.com',
+      url='https://github.com/youngrok/djangox-mako',
       version='1.0.0',
       packages=['djangoxmako', 
                 ],
