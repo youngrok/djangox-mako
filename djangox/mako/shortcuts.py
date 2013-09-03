a3b27eb33dcd9cb3fd37916c49d873c680b41f88
Pak Youngrok <pak.youngrok@gmail.com>
2013-09-03 15:00:35 +0900

    change path

--- djangox/mako/shortcuts.py
+++ djangox/mako/shortcuts.py
@@ -1,11 +1,11 @@
-import djangoxmako
+import djangox.mako
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseServerError
 from mako import exceptions
 
 def render_to_response(filename, dictionary, context_instance=None):
 
-    dictionary.update(djangoxmako.default_context)
+    dictionary.update(djangox.mako.default_context)
     
     if context_instance:
         for context_dict in context_instance.dicts:
@@ -15,7 +15,7 @@ def render_to_response(filename, dictionary, context_instance=None):
         dictionary.update(settings.MAKO_DEFAULT_CONTEXT)
 
     try:
-        template = djangoxmako.template_lookup.get_template(filename)
+        template = djangox.mako.template_lookup.get_template(filename)
         return HttpResponse(template.render(**dictionary))
     except:
         return HttpResponseServerError(exceptions.html_error_template().render())
