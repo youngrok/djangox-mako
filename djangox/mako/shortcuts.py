30c5eb5a0b4598b1bf8327251e5872f3fba3ea4c
Pak Youngrok <pak.youngrok@gmail.com>
2014-10-02 12:01:17 +0900

    remove shortcuts module, fix for python 3

--- djangox/mako/shortcuts.py
+++ djangox/mako/shortcuts.py
@@ -1,9 +1,22 @@
+import traceback
+import warnings
 import djangox.mako
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseServerError
 from mako import exceptions
 
+
 def render_to_response(filename, dictionary, context_instance=None):
+    '''
+    :param filename:
+    :param dictionary:
+    :param context_instance:
+    :return: rendered django HttpResponse
+
+    .. deprecated:: 1.07
+       Use :djangox.mako.render_to_response: instead.
+    '''
+    warnings.warn('use djangox.mako.render_to_response', DeprecationWarning)
 
     dictionary.update(djangox.mako.default_context)
     
@@ -18,4 +31,5 @@ def render_to_response(filename, dictionary, context_instance=None):
         template = djangox.mako.template_lookup.get_template(filename)
         return HttpResponse(template.render(**dictionary))
     except:
+        traceback.print_exc()
         return HttpResponseServerError(exceptions.html_error_template().render())
